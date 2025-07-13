835a5eb7c9784db65e17f46ebe1ef899a0f1cb31
Dilmer Ronald <dilmernotebook9@gmail.com>
2025-07-13 15:58:22 -0300

    Atualizado para teste

--- pingpong-menu/auto.js
+++ pingpong-menu/auto.js
@@ -1,1 +1,17 @@
+function startCampaing() {
+    alert("Iniciar Campanha");
+
+}
+
+function openOptions() {
+    alert("Abrir opções");
+}
+
+function showCredits() {
+    alert("Monstrar Créditos");
+}
+
+function exitGame() {
+    alert("Sair fo Jogo");
+}
 
